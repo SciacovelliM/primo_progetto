4ae3df91eb44955c36fb28694742aa36c3b14b0e
SciacovelliMarco <marksciak2014@gmail.com>
2020-12-09 12:23:23 +0100

    ..

--- news/urls.py
+++ news/urls.py
@@ -4,4 +4,5 @@ app_name = 'news'
 urlpatterns = [
     path('', home, name='homeview'),
     path('articoli/<int:pk>', ArticoloDetailViewCB.as_view(), name='articolo_detail')
+    path('lista_articoli/', ArticoloListView.as_view(), name='lista_articoli')
 ]
