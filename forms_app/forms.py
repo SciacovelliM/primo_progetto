4f022edf021880059ba7c162f798f73327cebb36
SciacovelliMarco <marksciak2014@gmail.com>
2021-03-24 13:17:12 +0100

    L26

--- forms_app/forms.py
+++ forms_app/forms.py
@@ -11,3 +11,17 @@ class FormContatto(forms.ModelForm):
     class Meta:
         model = Contatto
         fields = "__all__"
+        widgets={
+            'nome': forms.TextInput(attrs={'placeholder': 'Compila questo campo', 'class': 'form-control'}),
+            'cognome': forms.TextInput(attrs={'placeholder': 'Compila questo campo', 'class': 'form-control'}),
+            'email': forms.TextInput(attrs={'placeholder': 'Compila questo campo', 'class': 'form-control'}),
+            'contenuto': forms.TextInput(attrs={'placeholder': 'Compila questo campo', 'class': 'form-control'}),
+        }
+    
+    def clean_contenuto(self):
+        dati= self.cleaned_data["contenuto"]
+        if "parola" in dati:
+            raise ValidationError("Il contenuto inserito viola le norme del sito.")
+        if len(dati)<20:
+            raisen ValidationError("Il contenuto inserito è troppo breve")
+        return dati
